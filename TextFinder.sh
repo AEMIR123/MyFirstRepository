68b2446861f79a046761970242426da8f74d86ce
Stanisław Matyszkiel <140174656+AEMIR123@users.noreply.github.com>
2023-08-02 21:19:04 +0200

    Update TextFinder.sh

--- TextFinder.sh
+++ TextFinder.sh
@@ -1,1 +1,14 @@
-
+file=$1
+if [ -f  "$file" ]; then
+c=0
+while read words; do
+for word in ${words[@]}; do
+if [ "$word" == "$2" ]; then
+((c++))
+fi
+done
+done < $file
+echo "there are $c words"
+else
+echo "there is not such directory"
+fi
